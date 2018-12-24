38b4b751998619202a4c86c62edd4798d1f666ec
Nikita Riabchenko <unrealgear@gmail.com>
2018-12-24 17:13:38 +0200

    Added examples for JS file.

---
+++ source/js/testdata.js
@@ -0,0 +1,18 @@
+module.exports = {
+    users: {
+        user1: {
+            firstName: "Jake",
+            lastName: "Marko",
+            address: 1
+        },
+        user2: {
+            firstName: "Kate",
+            lastName: "Blanket",
+            address: 2
+        }
+    },
+    addresses: {
+        one: "New York",
+        two: "Seattle"
+    }
+};
